726819fa524ea9026bc5f5a34356e7c75a1fc8a8
Arman Bhalla <armanbhalla6@gmail.com>
2015-04-05 13:27:48 +0100

    Initial commit.
    Base files such as storyboard, launch screen, and app delegates.

---
+++ searchUITablesParse/searchUITablesParse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  searchUITablesParse
+//
+//  Created by Arman Bhalla on 05/04/2015.
+//  Copyright (c) 2015 Arman Bhalla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ searchUITablesParse/searchUITablesParse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  searchUITablesParse
+//
+//  Created by Arman Bhalla on 05/04/2015.
+//  Copyright (c) 2015 Arman Bhalla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
